e6606a4c000c17b86e1b7a96c36352cc51b154f4
JoshdlC <josue2003@gmail.com>
2024-09-24 20:06:25 -0600

    Flask

--- src/my-app/Hello.py
+++ src/my-app/Hello.py
@@ -1,13 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 
 
 app = Flask(__name__)
+app.config.from_mapping(
+    SECRET_KEY = 'dev'
+)
+
+#* Filtros 
+@app.add_template_filter
+def today(date):
+    return date.strftime('%d/%m/%Y')
+
+@app.add_template_global
+def repeat(text, times):
+    return text * times
+
+# @app.add_template_global(repeat, 'repeat')
+
+
+
+from datetime import datetime
 
 @app.route('/')
-@app.route('/index')
 def index():
-    name = None
-    return render_template('index.html', name = name)
+    print(url_for('index'))
+    print(url_for('hello', name='Josue', age=21))
+    print(url_for('code', code = 'print("Hello Worldsss")'))
+    friends = ['Juan', 'Pedro', 'Luis', 'Carlos', 'Ana']	
+    name = 'Josh'
+    date = datetime.now()
+    return render_template(
+        'index.html', 
+        name = name, 
+        friends = friends, 
+        date =date,
+    )
 
 
 #string
@@ -18,16 +45,51 @@ def index():
 @app.route('/hello')
 @app.route('/hello/<name>')
 @app.route('/hello/<name>/<int:age>')
+@app.route('/hello/<name>/<int:age>/<email>')
 
-def hello(name = None, age= None):
-    if name == None and age == None:
-        return '<h1>Hola, mundo</h1>'
-    elif age == None:
-        return f'<h1>Hola, {name}</h1>'
-    else: 
-        return f'<h1>Hola, {name} y tu edad es {age}</h1>'
+def hello(name = None, age= None, email = None):
+    my_data = {
+        'name': name,
+        'age': age,
+        'email': email 
+    }
+    return render_template('hello.html', data = my_data)
     
 from markupsafe import escape
 @app.route('/code/<path:code>')
 def code(code):
     return f'<code>El código es: {escape(code)}</code>'
+
+
+#*Formulario wtf-forms
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+class RegisterForm(FlaskForm):
+    username = StringField('Nombre de usuario: ', validators=[DataRequired(), Length(min=4, max=25)])
+    password = PasswordField('Contraseña: ', validators=[DataRequired(), Length(min=6, max=30)])
+    submit = SubmitField('Registrar:')
+
+
+#*registrar usuario
+@app.route('/auth/register', methods=['GET', 'POST'])   
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        return f"Nombre de usuario: {username}, Contraseña: {password}"
+    
+    # if request.method == 'POST':
+    #     username = request.form['username']
+    #     password = request.form['password']
+    #     # email = request.form['email']
+    #     if len(username) <= 25 and len(username) > 4 and len(password) >= 6 and len(password) <= 25:
+    #         return f"Nombre de usuario: {username}, Contraseña: {password}"
+    #     else:
+    #         error = """"Nombre de usuario debe de tener entre 4 y 25 caracteres y
+    #         la contraseña debe de tener entre 6 y 25 caracteres 
+    #         """
+    #         return render_template('auth/register.html', form = form, error=error)
+    return render_template('auth/register.html', form = form)
